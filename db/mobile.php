7c5b5fbd596ce0f5cf319b32a5db414274150d85
Jan Swanevelder <jan.swanevelder@getsmarter.com>
2019-08-30 14:25:35 +0200

    CTED-593 | Visible groups hide like

--- db/mobile.php
+++ db/mobile.php
@@ -35,7 +35,7 @@ $addons = array(
                 'init' => 'app_init', // Function that will run on runtime of the app
                 'styles' => array(
                     'url' => $CFG->wwwroot . '/mod/hsuforum/styles_app.css',
-                    'version' => '2019080700'
+                    'version' => '2019300800'
                 ),
             )
         ),
