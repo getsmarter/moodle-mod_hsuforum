171556ae1278cb3a819391b911f855f36bfcd9ee
Brendon Pretorius <bpretorius@2u.com>
2021-07-13 10:48:17 +0200

    Unminify JSW

--- amd/src/mod_hsuforum_button_animate.js
+++ amd/src/mod_hsuforum_button_animate.js
@@ -1,1 +1,22 @@
-define(["jquery"],function(e){return{init:function(r,n){e(".hsuforum-toggle").not(".hsuforum-toggle-bookmark, .hsuforum-toggle-substantive").hover(function(e){if(e.currentTarget.classList.contains("hsuforum-toggled")){if(e.currentTarget.children[0].children[0].innerText==n.toUpperCase()){e.currentTarget.children[0].children[0].innerText=r}else if(e.currentTarget.children[0].children[0].innerText==r.toUpperCase()){e.currentTarget.children[0].children[0].innerText=n}}}).mouseleave(function(e){if(e.currentTarget.classList.contains("hsuforum-toggled")){e.currentTarget.children[0].children[0].innerText=r}})}}});
+define(['jquery'], function ($) {
+    return {
+        init: function (following, unfollow) {
+
+            $('.hsuforum-toggle').not('.hsuforum-toggle-bookmark, .hsuforum-toggle-substantive')
+                .hover(function (e) {
+                    if (e.currentTarget.classList.contains('hsuforum-toggled')) {
+                        if (e.currentTarget.children[0].children[0].innerText == unfollow.toUpperCase()) {
+                            e.currentTarget.children[0].children[0].innerText = following
+                        } else if (e.currentTarget.children[0].children[0].innerText == following.toUpperCase()) {
+                            e.currentTarget.children[0].children[0].innerText = unfollow
+                        }
+                    }
+                })
+                .mouseleave(function (e) {
+                    if (e.currentTarget.classList.contains('hsuforum-toggled')) {
+                        e.currentTarget.children[0].children[0].innerText = following
+                    }
+                });
+        }
+    }
+})
