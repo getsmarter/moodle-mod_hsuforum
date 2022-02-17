d472f1bdd2103ea42d02790644098f65b2f2d526
Mmahadinana <mmohohlo-marumo@2u.com>
2022-02-17 07:52:42 +0200

    CTED-2311 | Cleaning the code

--- amd/src/mod_hsuforum_clear_contenteditable.js
+++ amd/src/mod_hsuforum_clear_contenteditable.js
@@ -3,13 +3,6 @@
  * Small JS file to mark all posts as read.
  */
 define(['jquery'], function($) {
-    const copyText = (event) => {
-        // eslint-disable-next-line no-console
-        console.log(event);
-        document.getSelection();
-        event.clipboardData.setData('text/plain');
-        event.preventDefault();
-    };
     return {
         init: function() {
             $('.posting').on('copy',function (event) {
