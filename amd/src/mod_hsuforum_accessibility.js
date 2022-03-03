43a2c10d026ffddda9c8cb16fdb1a559f50ea457
Brendon Pretorius <bpretorius@2u.com>
2022-03-03 07:17:13 +0200

    Accessibility bug fixes

--- amd/src/mod_hsuforum_accessibility.js
+++ amd/src/mod_hsuforum_accessibility.js
@@ -2,13 +2,21 @@
  * Small function that ties in with bootsrap collapse event classes to scroll a element
  * into view if not on the viewport
  */
- define(['jquery'], function($) {
-
+define(['jquery'], function($) {
     return {
         init: function() {
             $('body').on('click', '.hsuforum-cancel', function() {
                 $('.hsuforum-add-discussion input').focus();
             });
+
+            // // Force focus on clicked element
+            // $('body').on('click', '.hsuforum-tools a.like', function() {
+            //     let id = $(this).attr('id');
+            //     document.body.addEventListener("spinnerStopEvent", () => {
+            //         $('#' + id).focus();
+            //     });
+            //     // $('#' + id).focus();
+            // });
         }
     };
 });
