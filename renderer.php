61cc6b7d20f92bbb7207aaa06ffb91a2b3f1d518
Loyiso Ngqwebo <71312749+loyisongq@users.noreply.github.com>
2022-04-11 17:02:51 +0200

    CTED-2379 hsu forum labels update (#211)
    * CTED-2379 hsu forum labels update
    
    * CTED-2375 codechecker lin exceeds 132 characters fix

--- renderer.php
+++ renderer.php
@@ -2042,10 +2042,12 @@ HTML;
             $postuser   = hsuforum_extract_postuser($post, $forum, context_module::instance($cm->id));
             $commands['reply'] = html_writer::link(
                 new moodle_url('/mod/hsuforum/post.php', array('reply' => $post->id)),
-                '<i class="fa fa-reply fa-2"><span class="hsuforum-action-label" title="reply">' . get_string('replylabel', 'hsuforum') . '</span></i>',
+                '<i class="fa fa-reply fa-2"><span class="hsuforum-action-label" title="reply" aria-label="'
+                . get_string('replylabel', 'hsuforum'). "-" . $post->subject.'">
+                ' . get_string('replylabel', 'hsuforum') . '</span></i>',
                 array (
                     'class' => 'hsuforum-reply-link',
-                    'aria-label' => get_string('replylabel', 'hsuforum'). "-" . $post->id,
+                    'aria-label' => get_string('replylabel', 'hsuforum'). " " . $post->message,
                     'role' => 'link'
                 )
             );
